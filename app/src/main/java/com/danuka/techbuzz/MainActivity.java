dd5904f39f3ad5041ca09ede25fba2fd87744da8
Dhanuka <2022t01555@stu.cmb.ac.lk>
2025-06-03 01:44:08 +0530

    Created Side bar menu

--- app/src/main/java/com/danuka/techbuzz/MainActivity.java
+++ app/src/main/java/com/danuka/techbuzz/MainActivity.java
@@ -1,19 +1,60 @@
 package com.danuka.techbuzz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageView menuIcon;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        menuIcon = findViewById(R.id.menuIcon);
+        navigationView = findViewById(R.id.navigation_view);
+
+        // Open drawer when clicking menu icon
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        // Handle navigation item clicks
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_user_info) {
+                    // Open User Info activity
+                    //Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+                    //startActivity(intent);
+                } else if (id == R.id.nav_developer_info) {
+                    // Open Developer Info activity
+                    //Intent intent = new Intent(MainActivity.this, DeveloperInfoActivity.class);
+                    //startActivity(intent);
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
     }
 }
